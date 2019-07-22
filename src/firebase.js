0b61405dbdbe8a973f8d0648aaf5237d26c9f980
Erika Sanchez <lilixxxzaz@gmail.com>
2019-07-22 15:33:24 -0500

    Se agrega carpeta de fuentes de iconos y campos para registro con google

--- src/firebase.js
+++ src/firebase.js
@@ -15,3 +15,112 @@
         let bigOne = document.getElementById('bigOne');
         let dbRef = firebase.database().ref().child('text');
         dbRef.on('value', snap => bigOne.innerText = snap.val());
+
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+        function handleSignUp() {
+          var email = document.getElementById('email').value;
+          var password = document.getElementById('password').value;
+          if (email.length < 4) {
+            alert('Please enter an email address.');
+            return;
+          }
+          if (password.length < 4) {
+            alert('Please enter a password.');
+            return;
+          }
+          // Sign in with email and pass.
+          // [START createwithemail]
+          firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // [START_EXCLUDE]
+            if (errorCode == 'auth/weak-password') {
+              alert('The password is too weak.');
+            } else {
+              alert(errorMessage);
+            }
+            console.log(error);
+            // [END_EXCLUDE]
+          });
+          // [END createwithemail]
+        }
+        function initApp() {
+          // Listening for auth state changes.
+          // [START authstatelistener]
+          firebase.auth().onAuthStateChanged(function(user) {
+            // [START_EXCLUDE silent]
+            
+            // [END_EXCLUDE]
+            if (user) {
+              // User is signed in.
+              var displayName = user.displayName;
+              var email = user.email;
+              var emailVerified = user.emailVerified;
+              var photoURL = user.photoURL;
+              var isAnonymous = user.isAnonymous;
+              var uid = user.uid;
+              var providerData = user.providerData;
+              // [START_EXCLUDE]
+             
+              document.getElementById('enterWithGoogle').textContent = 'Sign out';
+              
+              // [END_EXCLUDE]
+            } 
+            // [START_EXCLUDE silent]
+           
+            // [END_EXCLUDE]
+          });
+          // [END authstatelistener]
+          
+          document.getElementById('enterWithGoogle').addEventListener('click', handleSignUp, false);
+         
+        }
+        window.onload = function() {
+          initApp();
+        };
+       
+    
+/*const data = firebase.database();
+let data = {
+    "Moscow": {
+      country: "Russia"
+    },
+    "Berlin": {
+      name: "Germany"
+    }
+  }
+//Obtener una referencia a la raíz de la base de datos
+let refToData = data.ref('cities'); 
+//Obtener una console.log de todos los datos 
+dataRef.once('value', snapshot => {
+  console.log(snapshot.val());
+});
+let dataPush = dataRef.push(data);*/
+
+        
--- src/main.js
+++ src/main.js
@@ -1,22 +1,36 @@
 document.getElementById("dataLogin").style.display="block";
-document.getElementById("email").value="";
-document.getElementById("password").value="";
+//document.getElementById("email").value="";
+//document.getElementById("password").value="";
+
 const enterLogin =()=>{
     document.getElementById("newUser").style.display = "block";
     document.getElementById("dataLogin").style.display = "none";
 }
 document.getElementById("register").addEventListener("click", enterLogin);
 
-const enterGoogle=()=>{
-firebase.auth().signInWithEmailAndPassword(email, password).catch(error => {
-// Handle Errors here.
-let errorCode = error.code;
-let errorMessage = error.message;
-console.log(errorCode);
-console.log(errorMessage);
-    }); 
-  };
-  
-document.getElementById("loginGoogle").addEventListener("click", enterGoogle);
 
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+const enterGoogle=()=>{
+  document.getElementById("googleUser").style.display = "block";
+  document.getElementById("dataLogin").style.display = "none";
+  };
+document.getElementById("loginGoogle").addEventListener("click", enterGoogle);
+ 
+const sendHome=()=>{
+  document.getElementById("googleUser").style.display = "none";
+  document.getElementById("dataLogin").style.display = "none";
+  document.getElementById("home").style.display = "block";
+  };
+document.getElementById("enterWithGoogle").addEventListener("click", sendHome);
