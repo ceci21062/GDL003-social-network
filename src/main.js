2d0979ac7321ae3af08385b1e57a827d5bf3e99a
Erika Sanchez <lilixxxzaz@gmail.com>
2019-07-25 14:37:40 -0500

    Se agrega imagen y diseño de home

--- src/main.js
+++ src/main.js
@@ -5,15 +5,15 @@ document.getElementById("confirmText").style.display = "none";
 document.getElementById("confirmPassword").style.display = "none"; 
 document.getElementById("createUser").style.display = "none";
 document.getElementById("enterWithGoogle").style.display = "none";
-
-
+document.getElementById("home").style.display = "none";
+document.getElementById("footerMenu").style.display = "none";
 
 const enterRegister = () => {
-  document.getElementById("nameText").style.display = "block";
-  document.getElementById("name").style.display = "block";
-  document.getElementById("confirmText").style.display = "block";
-  document.getElementById("confirmPassword").style.display = "block";
-  document.getElementById("createUser").style.display = "block";
+  document.getElementById("nameText").style.display = "inline-block";
+  document.getElementById("name").style.display = "inline-block";
+  document.getElementById("confirmText").style.display = "inline-block";
+  document.getElementById("confirmPassword").style.display = "inline-block";
+  document.getElementById("createUser").style.display = "inline-block";
   document.getElementById("login").style.display = "none";
   document.getElementById("loginGoogle").style.display = "none";
   document.getElementById("register").style.display = "none";
@@ -23,9 +23,9 @@ const enterRegister = () => {
 document.getElementById("register").addEventListener("click", enterRegister);
  
 const sendHome = () => {
-  document.getElementById("googleUser").style.display = "none";
-  document.getElementById("dataLogin").style.display = "block";
+  document.getElementById("dataLogin").style.display = "none";
   document.getElementById("home").style.display = "block";
+  
 };
-document.getElementById("enterWithGoogle").addEventListener("click", sendHome);
+document.getElementById("login").addEventListener("click", sendHome);
 
